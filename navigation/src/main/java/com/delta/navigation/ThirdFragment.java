6d7e8a7e76253317ab8f9813def8242994506733
learnovate <spawrks@learnovatelabs.com>
2014-04-11 18:00:40 -0500

    .gitignore file added

---
+++ navigation/src/main/java/com/delta/navigation/ThirdFragment.java
@@ -0,0 +1,17 @@
+package com.delta.navigation;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by learnovate on 4/11/14.
+ */
+public class ThirdFragment extends Fragment {
+
+}
