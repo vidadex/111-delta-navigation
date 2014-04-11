6848653f2e48a44ccf8080fc7452861ba32d5c68
learnovate <spawrks@learnovatelabs.com>
2014-04-11 18:02:47 -0500

    .gitignore file added

--- navigation/src/main/java/com/delta/navigation/NavigationActivity.java
+++ navigation/src/main/java/com/delta/navigation/NavigationActivity.java
@@ -20,62 +20,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class NavigationActivity extends Activity implements ActionBar.TabListener{
+public class NavigationActivity extends Activity{
 
-
-
-    private ViewPager viewPager;
-    private TabsAdapter mAdapter;
-    private android.app.ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        actionBar = getActionBar();
-        mAdapter = new TabsAdapter(getFragmentManager());
-
-        viewPager.setAdapter(mAdapter);
-        actionBar.setHomeButtonEnabled(false);
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        //adding tabs
-        for (String tabName : TabsAdapter.getTabNames()) {
-            ActionBar.Tab aTab = actionBar.newTab()
-                                          .setText(tabName)
-                                          .setTabListener(this);
-            actionBar.addTab(aTab);
-        }
-
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
-            }
-        });
-
-
-
-
-
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
@@ -83,35 +40,7 @@ public class NavigationActivity extends Activity implements ActionBar.TabListene
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                Toast.makeText(this,"Selected Settings",Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.action_rategood:
-                Toast.makeText(this,"This App Rules!", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-        viewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
+        return super.onOptionsItemSelected(item);
     }
 
 }
